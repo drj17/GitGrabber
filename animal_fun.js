d3eca407399bc8b901489b15fc6eef524f054602
David Janas <davidjanasr@gmail.com>
2017-10-23 15:48:52 -0400

    basic server that responds to querystrings

--- animal_fun.js
+++ animal_fun.js
@@ -1,21 +1,46 @@
-const fs = require('fs')
-let letter = process.argv[2].toUpperCase()
-fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-  if(err){
-    console.log(err)
-    return
-  }
-  let lines = data.split("\n")
-  let firstLetterWords = lines.filter((word) => {
-    return word[0] === letter;
-  })
+const fs = require('fs');
+const http = require('http');
+const querystring = require('querystring');
 
-  fs.writeFile(`${letter}-animals.txt`, firstLetterWords, err => {
-    if (err) {
-      console.log(err)
-      return
+const server = http.createServer((req, res) => {
+  const query = req.url.split('?')[1];
+  const parsed = querystring.parse(query);
+  const letter = parsed['letter'];
+
+  fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+    if(err){
+      console.log(err);
+      return;
     }
-    console.log("file successfully wrriten")
-  })
+    let lines = data.split("\n");
+    let firstLetterWords = lines.filter((word) => {
+      return word[0] === letter.toUpperCase();
+    });
+    console.log(firstLetterWords);
+    res.end(firstLetterWords.join('\n'));
+  });
+});
 
-})
+server.listen(8000, () => console.log("I'm listening on port 8000!"));
+
+
+// let letter = process.argv[2].toUpperCase()
+// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+//   if(err){
+//     console.log(err)
+//     return
+//   }
+//   let lines = data.split("\n")
+//   let firstLetterWords = lines.filter((word) => {
+//     return word[0] === letter;
+//   })
+//
+//   fs.writeFile(`${letter}-animals.txt`, firstLetterWords, err => {
+//     if (err) {
+//       console.log(err)
+//       return
+//     }
+//     console.log("file successfully wrriten")
+//   })
+//
+// })
